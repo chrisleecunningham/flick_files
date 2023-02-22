c72baf958b57ed7eb496be5e0fd55d78ab5f9e93
chrisleecunningham <103608696+chrisleecunningham@users.noreply.github.com>
2023-02-22 14:59:06 -0800

    Update index.js
    corrected typo

--- index.js
+++ index.js
@@ -204,7 +204,7 @@ app.post('/users/:id/:movieTitle', (req, res) => {
 
     if (user) {
         user.favoriteMovies.push(movieTitle);
-        res.status(200).json(user);
+        res.status(200).send('success!');
     }   else {
         res.status(400).send('No such user');
     }
