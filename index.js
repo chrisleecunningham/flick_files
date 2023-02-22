6a342d892e11126b57ec5b4afb07f8d88043ad9d
chrisleecunningham <103608696+chrisleecunningham@users.noreply.github.com>
2023-02-22 15:13:03 -0800

    Update index.js
    removing movie function added

--- index.js
+++ index.js
@@ -204,7 +204,23 @@ app.post('/users/:id/:movieTitle', (req, res) => {
 
     if (user) {
         user.favoriteMovies.push(movieTitle);
-        res.status(200).send('success!');
+        res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
+    }   else {
+        res.status(400).send('No such user');
+    }
+});
+
+
+// DELETE favoriteMovies under user function
+app.delete('/users/:id/:movieTitle', (req, res) => {
+    const { id, movieTitle } = req.params;
+    const updatedUser = req.body;
+
+    let user = users.find( user => user.id == id );
+
+    if (user) {
+        user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
+        res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
     }   else {
         res.status(400).send('No such user');
     }
