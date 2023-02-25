21acccd4d5b72d3c202d0d5b8582dec53fd325dd
chrisleecunningham <103608696+chrisleecunningham@users.noreply.github.com>
2023-02-24 16:01:49 -0800

    Update models.js
    removed quotes, corrected comma before imagePath

--- models/models.js
+++ models/models.js
@@ -1,28 +1,28 @@
 const mongoose = require('mongoose');
 
 let movieSchema = mongoose.Schema({
-    "title": {type: String, required: true},
-    "description": {type: String, required: true},
-    "genre": {
-        "name": String,
-        "description": String
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    genre: {
+        name: String,
+        description: String
     },
-    "director": {
-        "name": String,
-        "bio": String,
-        "birth": Date
+    director: {
+        name: String,
+        bio: String,
+        birth: Date
     },
-    "actors": [String]
-    "imagePath": String,
-    "featured": Boolean
+    actors: [String],
+    imagePath: String,
+    featured: Boolean
 });
 
 let userSchema = mongoose.Schema({
-    "username": {type: String, required: true},
-    "password": {type: String, required: true},
-    "email": {type: String, required: true},
-    "birthDate": Date,
-    "favoriteMovies": [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true},
+    birthDate: Date,
+    favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
 let Movie = mongoose.model('Movie', movieSchema);
